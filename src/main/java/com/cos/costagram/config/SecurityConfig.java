40dbfa7a45bdc0b699b5daddd35beb1b48446a53
lonru <lonru@deu.ac.kr>
2021-03-23 09:26:40 +0900

    4.loginForm joinForm update

--- src/main/java/com/cos/costagram/config/SecurityConfig.java
+++ src/main/java/com/cos/costagram/config/SecurityConfig.java
@@ -1,5 +1,36 @@
 package com.cos.costagram.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	//모델 : Image, User,Likes,Follow, Tag 인증 필요함
+	// auth 주소, 인증필요없음
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/", "/user/**", "/image/**", "/follow/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+		.anyRequest()
+		.permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/auth/loginForm")
+		.loginProcessingUrl("/login")//post /login주소를 디스패처 확인하면 필터가 낚아챔
+		.defaultSuccessUrl("/");
+		
+//		http.cors().disable(); //컨트롤러에서 CrossOrigin을 붙여도 시큐리티에서 막힘.
+	}
 }
